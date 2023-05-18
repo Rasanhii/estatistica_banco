8500b4d00681ad22d1d31cc44b5c8727b054ffcb
EC2 Default User <ec2-user@ip-172-31-55-28.ec2.internal>
2023-05-18 22:17:30 +0000

    final

--- tribo_brasil/completo.py
+++ tribo_brasil/completo.py
@@ -54,6 +54,32 @@ def deletar(codigo):
     cursor.close()
     conn.close()
 
+
+def atualizar(nome_tribo, num_hab, renda_mensal, escolaridade, trab_salar):
+    # Abrir uma conexão com o banco de dados
+    conn = conectar()
+
+    # Criando um objeto cursor para executar as consultas SQL
+    cursor = conn.cursor()
+
+    # Executar a consulta SQL para atualizar o registro
+    sql = "UPDATE tribo SET nome_tribo = %s, num_hab= %s, renda_mensal= %s, escolaridade= %s, trab_salar = %s WHERE id = %s"
+    val = (nome_tribo, num_hab, renda_mensal, escolaridade, trab_salar)
+    cursor.execute(sql, val)
+
+    # Commit da transação
+    conn.commit()
+
+    # Verificar se algum registro foi atualizado
+    if cursor.rowcount == 0:
+        print("Nenhum registro atualizado.")
+    else:
+        print("Registro atualizado com sucesso.")
+
+    # Fechar a conexão e o cursor
+    cursor.close()
+    conn.close()
+
 conn = conectar()
 
 cursor = conn.cursor()
@@ -64,16 +90,17 @@ while True:
   print("1 - Listar tribos")
   print("2 - Inserir nova tribo")
   print("3 - Deletar uma tribo")
+  print("4 - Atualizar uma tribo")
   print("0 - Sair")
   
   opcao = int(input("Digite o número da opção desejada: "))
 
   if opcao == 1:
-    # Listar estados
+    # Listar tribos
     listar(conn, cursor)
   
   elif opcao == 2:
-    # Inserir novo estado
+    # Inserir novo tribo
     nome_tribo = input("Digite nome da nova tribo: ")
     num_hab = int(input("Digite o num de habitantes da nova tribo: "))
     renda_mensal = input("Digite a renda mensal: ")
@@ -83,9 +110,20 @@ while True:
     inserir(nome_tribo, num_hab, renda_mensal, escolaridade, trab_salar)
 
   elif opcao == 3:
-    # Deletar um estado
+    # Deletar um tribo
     codigo = int(input("Digite o código da tribo que deseja deletar: "))
     deletar(codigo)
+
+
+  elif opcao == 4:
+    # Atualizar um tribo
+    nome_tribo = input("Digiteo nome da nova tribo: ")
+    num_hab = int(input("Digite o num de habitantes da nova tribo: "))
+    renda_mensal = input("Digite a renda mensal: ")
+    escolaridade = input("Digite a escolaridade da nova tribo: ")
+    trab_salar = input("Digite se possuem trabalho assalariado: ")
+    atualizar(nome_tribo, num_hab, renda_mensal, escolaridade, trab_salar)
+  
 
   elif opcao == 0:
     # Sair do programa
